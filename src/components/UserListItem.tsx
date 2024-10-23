0429ca347b9b2a38e6c0554b65db446bdcfbc3b6
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-23 19:16:02 -0300

    feat(panel): create proto expandable panel=>
    -used in UserListItem
    -open and closing still in development

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -2,6 +2,7 @@ import Button from "./Button";
 import { GoTrashcan } from "react-icons/go";
 import { useThunkWithArg } from "../hooks/use-thunk";
 import { deleteUser } from "../thunks/deleteUser";
+import ExpandablePanel from "./ExpandablePanel";
 
 function UserListItem({ user }: { user: User }) {
   const [removeUser, removingUser, removeUserError] =
@@ -11,26 +12,27 @@ function UserListItem({ user }: { user: User }) {
     removeUser(id);
   };
 
+  const header = (
+    <>
+      <div className=" scale-75">
+        <Button
+          className="mr-3"
+          outline
+          loading={removingUser}
+          onClick={() => handleRemove(user.id)}
+        >
+          <GoTrashcan className="scale-125" />
+        </Button>
+      </div>
+
+      {user.name}
+    </>
+  );
+
   return removeUserError ? (
     <div>Error Removing User</div>
   ) : (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between ">
-          <div className=" scale-75">
-            <Button
-              className="mr-3"
-              outline
-              loading={removingUser}
-              onClick={() => handleRemove(user.id)}
-            >
-              <GoTrashcan className="scale-125" />
-            </Button>
-          </div>
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>CONTENT</ExpandablePanel>
   );
 }
 
