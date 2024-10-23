262378f4fd4800e85b9ab64973cd7bdd3d587ca7
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-22 23:59:01 -0300

    style(users): change variable names for errors

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -10,8 +10,8 @@ function UserList() {
   const { data } = useSelector((state: RootState) => {
     return state.users;
   });
-  const [loadUsers, loadingUsers, loadingUsersError] = useThunk(fetchUsers);
-  const [createUser, creatingUser, creatingUserError] = useThunk(addUser);
+  const [loadUsers, loadingUsers, loadUsersError] = useThunk(fetchUsers);
+  const [createUser, creatingUser, createUserError] = useThunk(addUser);
 
   const handleAddUser = () => {
     createUser();
@@ -21,31 +21,36 @@ function UserList() {
     loadUsers();
   }, [loadUsers]);
 
-  const renderedUsers = data.map((user) => (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
-      </div>
+  const alwaysShow = (
+    <div className="flex flex-row justify-between items-center m-3">
+      <h1 className="m-2 text-xl">Users</h1>
+      <Button onClick={handleAddUser} loading={creatingUser || loadingUsers}>
+        + Add User
+      </Button>
+      {createUserError && <div>Error creating user</div>}
     </div>
-  ));
+  );
+
+  const variableContent = loadingUsers ? (
+    <div>
+      <Skeleton times={7} className="h-10 w-full" />
+    </div>
+  ) : loadUsersError ? (
+    <div>Error fetching data...</div>
+  ) : (
+    data.map((user) => (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    ))
+  );
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
-        <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleAddUser} loading={creatingUser || loadingUsers}>
-          + Add User
-        </Button>
-        {creatingUserError && <div>Error creating user</div>}
-      </div>
-
-      {loadingUsers ? (
-        <Skeleton times={7} className="h-10 w-full" />
-      ) : loadingUsersError ? (
-        <div>Error fetching data...</div>
-      ) : (
-        renderedUsers
-      )}
+      {alwaysShow}
+      {variableContent}
     </div>
   );
 }
