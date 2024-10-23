17f8f06bd0e066d727ca28179a4222ccc9cccd2d
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-23 19:30:10 -0300

    feat(panel): make it foldable/expandable

--- src/components/ExpandablePanel.tsx
+++ src/components/ExpandablePanel.tsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
+
 type ExpandablePanelProps = {
   header: React.ReactNode;
   children: React.ReactNode;
 };
 
 function ExpandablePanel({ header, children }: ExpandablePanelProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpenClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div
+        onClick={handleOpenClick}
+        className="flex p-2 justify-between items-center cursor-pointer"
+      >
         <div className="flex flex-row items-center justify-between ">
           {header}
         </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {isOpen && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 }
