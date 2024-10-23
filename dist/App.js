f2f9a6e34b169aab4467f0d4044bb65fe1bd7691
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-23 18:47:31 -0300

    chore(git): update git ignoring

--- dist/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import UserList from "./components/UserList";
-function App() {
-    return (_jsx("div", { children: _jsx(UserList, {}) }));
-}
-export default App;
-//# sourceMappingURL=App.js.map
--- dist/hooks/use-pause.js
+++
@@ -1,7 +0,0 @@
-const usePause = (duration) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, duration);
-    });
-};
-export { usePause };
-//# sourceMappingURL=use-pause.js.map
