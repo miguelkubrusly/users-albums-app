6939cf897112ba68c25d2a1457fce09b9a975a0e
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-23 21:45:36 -0300

    feat(panel): add icon & change state name

--- src/components/ExpandablePanel.tsx
+++ src/components/ExpandablePanel.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
 type ExpandablePanelProps = {
   header: React.ReactNode;
@@ -6,23 +7,24 @@ type ExpandablePanelProps = {
 };
 
 function ExpandablePanel({ header, children }: ExpandablePanelProps) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [expanded, setExpanded] = useState<boolean>(false);
 
   const handleOpenClick = () => {
-    setIsOpen(!isOpen);
+    setExpanded(!expanded);
   };
 
   return (
     <div className="mb-2 border rounded">
       <div
         onClick={handleOpenClick}
-        className="flex p-2 justify-between items-center cursor-pointer"
+        className="flex justify-between items-center cursor-pointer"
       >
         <div className="flex flex-row items-center justify-between ">
           {header}
         </div>
+        {expanded ? <GoChevronDown /> : <GoChevronLeft />}
       </div>
-      {isOpen && <div className="p-2 border-t">{children}</div>}
+      {expanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 }
